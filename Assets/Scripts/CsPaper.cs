55af97093c2c1f84310cad9080923e5aed49b22e
whirlwind_ko <rladyddnrl@gmail.com>
2015-05-10 20:20:43 +0900

    Initial Commit.

---
+++ Assets/Scripts/CsPaper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CsPaper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Ray ray;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        
+
+	}
+    void OnMouseDown()
+    {
+        Vector3 a = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        a.Set(a.x, a.y, gameObject.transform.position.z);
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, a, 1);
+    }
+
+}
